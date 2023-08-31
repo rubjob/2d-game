656dd0de965e02fc4c9083d49e74aa2f9cb4666f
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2023-08-31 13:42:48 +0700

    feat: add idle and walk player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,29 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 velocity = Vector2.zero;
-
+    
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); 
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     
     private void FixedUpdate()
     {
+        // Movement
         velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         velocity = velocity.normalized * movementSpeed;
 
         rb.velocity = velocity;
+
+        // Animation
+        Boolean isMoving = velocity != Vector2.zero;
+        animator.SetBool("isMoving", isMoving); 
+        
+        if (isMoving) {
+            spriteRenderer.flipX = velocity.x < 0;
+        }
     }
 }
