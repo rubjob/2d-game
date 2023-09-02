ae40433ccbc25171185756b065af6eec314d2179
oreo10baht <111551012+oreo10baht@users.noreply.github.com>
2023-09-03 03:22:48 +0700

    feat: add attack animation

--- Assets/Scripts/PlayerActions/PrimaryAttack.cs
+++ Assets/Scripts/PlayerActions/PrimaryAttack.cs
@@ -2,24 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrimaryAttack : BasePlayerState {
+public class PrimaryAttack : BasePlayerState
+{
     public Animator animator;
     public SpriteRenderer spriteRenderer;
+    PlayerController playerController;
+    private Animator anim;
 
-    private void Update() {
+    int combo;
+
+
+
+    private void Start()
+    {
+        playerController = GameObject.FindGameObjectWithTag("GameController").GetComponent<PlayerController>();
+        anim = GetComponent<Animator>();
+        combo = 0;
+    }
+    private void Update()
+    {
         // Play animation
         Vector2 velocity = player.GetComponent<Rigidbody2D>().velocity;
         bool isMoving = velocity != Vector2.zero;
 
         animator.SetBool("isMoving", isMoving);
-        if (isMoving && velocity.x != 0) {
+        if (isMoving && velocity.x != 0)
+        {
             spriteRenderer.flipX = velocity.x < 0;
         }
     }
 
-    protected override void Action(GameObject target) {
-        if (target != null) {
+    protected override void Action(GameObject target)
+    {
+
+        if (combo == 0)
+        {
+            anim.SetTrigger("Attack");
+
+        }
+        if (target != null)
+        {
             target.GetComponent<BaseEntity>().takeDamage(attackDamage);
+
         }
     }
+    public void lockMovement()
+    {
+        playerController.movementSpeed = 0;
+        combo += 1;
+    }
+    public void unlockMovement()
+    {
+        playerController.movementSpeed = 5f;
+        combo -= 1;
+
+    }
+
 }
