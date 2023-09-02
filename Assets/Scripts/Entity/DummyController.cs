a158e972e31dabf0de7070f1fa030507015ee0cf
oreo10baht <111551012+oreo10baht@users.noreply.github.com>
2023-09-02 21:25:32 +0700

    feat: add entity hitbox

--- Assets/Scripts/Entity/DummyController.cs
+++ Assets/Scripts/Entity/DummyController.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DummyController : BaseEntity {
+public class DummyController : BaseEntity
+{
     public Animator animator;
 
-    protected override void OnTakenDamage(float amount) {
+    protected override void OnTakenDamage(float amount)
+    {
         Debug.Log("Damaged taken: " + amount);
         animator.SetTrigger("isDamaged");
     }
 
-    protected override void OnDead() {
+    protected override void OnDead()
+    {
         Debug.Log("IM DEAD ARRRRRRRRRRRRRRRRRRRRRR");
     }
 }
