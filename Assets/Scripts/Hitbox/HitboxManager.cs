29e591e890d4d5eee70d7d41f70991eb4d727641
oreo10baht <111551012+oreo10baht@users.noreply.github.com>
2023-10-06 16:54:58 +0700

    feat: add q anim

--- Assets/Scripts/Hitbox/HitboxManager.cs
+++ Assets/Scripts/Hitbox/HitboxManager.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HitboxManager : MonoBehaviour {
+public class HitboxManager : MonoBehaviour
+{
     [SerializeField] private HitboxTriggerer trigger;
-    public HitboxTriggerer Trigger {
+    public HitboxTriggerer Trigger
+    {
         get { return trigger; }
     }
 
-    public void RotateTo(float angle) {
+    public void RotateTo(float angle)
+    {
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
