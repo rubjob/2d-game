5372819e36d6bc9dca49fe627670b6232e3c0f1b
Thanapat Ussawanarong <boss.32560@gmail.com>
2023-10-12 16:35:24 +0700

    feat: add e scripts

---
+++ Assets/Scripts/PlayerActions/EAttack.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EAttack : BaseEntityState
+{
+    [Header("Dependency")]
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    [Header("Attack")]
+    [SerializeField] private HitboxManager hitbox;
+    [SerializeField] private float attackDamage = 20f;
+    [SerializeField] private float attackSpeed = 1.5f;
+    [SerializeField] private float cooldownTime = 1.5f;
+
+    [Header("Knockback")]
+    public float knockbackStrength = 5f;
+    public float knockbackDelay = 0.15f;
+
+    public override float AttackDamage => attackDamage;
+    public override float AttackSpeed => attackSpeed;
+    public override HitboxManager Hitbox => hitbox;
+    public override float CooldownDuration => cooldownTime;
+
+    public override IEnumerator OnPlayingAnimation() {
+        animator.speed = Mathf.Clamp(AttackSpeed, 1, float.MaxValue);
+        animator.SetTrigger("EAttack");
+
+        yield return new WaitForSeconds(1f / AttackSpeed);
+    }
+
+    public override void OnDealingDamage() {
+        GameObject[] targets = hitbox.Trigger.TriggeringObjects;
+
+        if (targets.Length > 0) {
+            for (int i = 0; i < targets.Length; i++) {
+                targets[i].GetComponent<HealthScript>().TakeDamage(AttackDamage);
+
+                KnockbackScript kb = targets[i].GetComponent<KnockbackScript>();
+                Vector2 direction = (kb.rb.position - rb.position).normalized;
+                kb.Knockback(direction, knockbackStrength, knockbackDelay);
+            }
+        }
+    }
+}
