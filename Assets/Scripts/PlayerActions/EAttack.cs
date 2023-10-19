267bd6199994b9b669692aa9c6218aa32804a426
Thanapat Ussawanarong <boss.32560@gmail.com>
2023-10-19 16:43:49 +0700

    feat: add damage pop up to e

--- Assets/Scripts/PlayerActions/EAttack.cs
+++ Assets/Scripts/PlayerActions/EAttack.cs
@@ -42,6 +42,8 @@ public class EAttack : BaseEntityState
                 KnockbackScript kb = targets[i].GetComponent<KnockbackScript>();
                 Vector2 direction = (kb.rb.position - rb.position).normalized;
                 kb.Knockback(direction, knockbackStrength, knockbackDelay);
+
+                DamagePopup.Create(kb.rb.position, AttackDamage);
             }
         }
     }
