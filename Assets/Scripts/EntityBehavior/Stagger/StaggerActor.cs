94617981e016995fd5ef48cdd327ba2735e84c8f
Thanapat Ussawanarong <boss.32560@gmail.com>
2023-10-19 21:05:34 +0700

    feat: add stagger scripts

---
+++ Assets/Scripts/EntityBehavior/Stagger/StaggerActor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StaggerActor : MonoBehaviour
+{
+    [Header("Constant")]
+    public int MaxStaggerLevel;
+
+    private int Level;
+
+    [Header("Events")]
+    public UnityEvent OnStagger;
+
+    public void AddStaggerLevel(int level) {
+        Debug.Log(level);
+
+        Level += level;
+        
+        if (Level >= MaxStaggerLevel) {
+            Level = 0;
+            OnStagger?.Invoke();
+        }
+    }
+}
