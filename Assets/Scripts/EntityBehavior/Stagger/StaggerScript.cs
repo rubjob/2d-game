dca765efb88cd06301d75b0a5c9bcd08e61a052d
Thanapat Ussawanarong <boss.32560@gmail.com>
2023-10-29 13:47:23 +0700

    fix: stagger attack script missing

--- Assets/Scripts/EntityBehavior/Stagger/StaggerScript.cs
+++ Assets/Scripts/EntityBehavior/Stagger/StaggerScript.cs
@@ -7,7 +7,7 @@ public class StaggerScript : MonoBehaviour
     [Header("Constant")]
     public int AttackStaggerStrength;
 
-    public void Attack(GameObject obj) {
+    public void Attack(GameObject obj, Vector2 direction) {
         StaggerActor actor;
         if (actor = obj.GetComponent<StaggerActor>()) {
             actor.AddStaggerLevel(AttackStaggerStrength);
