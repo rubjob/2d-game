f0bd02b5c7d6c36606f2c7b8cfcaae645e1b30b0
Thanapat Ussawanarong <boss.32560@gmail.com>
2023-11-04 11:45:25 +0700

    feat: add debuff

---
+++ Assets/Scripts/EntityBehavior/Debuff/DebuffActor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace EntityBehavior {
+    public class DebuffActor : MonoBehaviour
+    {
+        [Header("Dependency")]
+        public HealthScript HealthScript;
+
+        [Header("Debuff")]
+        public Debuff Debuff;
+
+        public void ApplyDebuff(Debuff Debuff) {
+            this.Debuff = Debuff;
+
+            StopAllCoroutines();
+            StartCoroutine(CoDebuffEffect());
+            StartCoroutine(CoDebuffDuration());
+        }
+
+        private IEnumerator CoDebuffEffect() {
+            yield return new WaitForFixedUpdate();
+
+            while (Debuff != null) {
+                HealthScript.TakeDamage(Debuff.Damage);
+                yield return new WaitForSeconds(Debuff.TriggerEvery);
+            }
+        }
+
+        private IEnumerator CoDebuffDuration() {
+            yield return new WaitForSeconds(Debuff.Duration);
+            Debuff = null;
+        }
+    }
+}
