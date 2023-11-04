d8cfc1906233f7e1c62d3e171a9be2ce79dee70f
Thanapat Ussawanarong <boss.32560@gmail.com>
2023-11-04 19:30:22 +0700

    feat: add poisonous smoke

--- Assets/Scripts/Hitbox/HitboxTriggerer.cs
+++ Assets/Scripts/Hitbox/HitboxTriggerer.cs
@@ -2,23 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HitboxTriggerer : MonoBehaviour
 {
+    [Header("Events")]
+    public UnityEvent<GameObject> OnObjectTriggered, OnObjectTriggering;
+
     private readonly HashSet<GameObject> triggeringObjects = new();
 
     public GameObject[] TriggeringObjects
     {
-        get { return triggeringObjects.ToArray<GameObject>(); }
+        get { return triggeringObjects.ToArray(); }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         triggeringObjects.Add(other.gameObject);
+        OnObjectTriggered?.Invoke(other.gameObject);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         triggeringObjects.Remove(other.gameObject);
+    }
+
+    private void FixedUpdate() {
+        if (OnObjectTriggering == null) return;
+
+        foreach (GameObject obj in triggeringObjects.ToArray()) {
+            OnObjectTriggering.Invoke(obj);
+        }
     }
 }
